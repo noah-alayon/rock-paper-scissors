2e8159fb6ce3f3ecedc94415a3da0f7d511d57c5
Noah Alayon <noahalayon01@gmail.com>
2026-07-27 18:40:56 -0400

    Add getComputerChoice()
    Add const int variables for ROCK, PAPER, and
    SCISSORS for getComputerChoice() to use and
    compare to Math.random() Number result

---
+++ script.js
@@ -0,0 +1,15 @@
+const ROCK = 0;
+const PAPER = 1;
+const SCISSORS = 2;
+
+function getComputerChoice() {
+    // Generate number between [0,2]
+    const choice = Math.floor(Math.random() * 3);
+
+    switch (choice) {
+        case ROCK:      return "rock";
+        case PAPER:     return "paper";
+        case SCISSORS:  return "scissors";
+        default:        return "N/A";
+    }
+}
